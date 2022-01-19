37aa5253a22922d05445f93b533e4ab82a21bb7e
Lenovo <somusp75@gmail.com>
2022-01-19 23:06:46 +0530

    Add project files.

---
+++ StockAccountManagement/Program.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+using System.IO;
+
+namespace StockAccountManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            //Creating obj for StockManager
+            StockManagement stockManagement = new StockManagement();
+            //getting path of json file
+            string file = @"D:\VisualStudio\repos\StockAccountManagement\StockAccountManagement\stock.json";
+            //DeserializeO Json file
+            StockSupport stockSupport = JsonConvert.DeserializeObject<StockSupport>(File.ReadAllText(file));
+            Console.WriteLine("Enter Your Option To Perform\n 1-Add a stock\n 2-Remove a stock\n3-Display Stocks");
+            int num = Convert.ToInt32(Console.ReadLine());
+            var fs = stockSupport.stocksList;
+            switch (num)
+            {
+                case 1:
+                    stockManagement.AddStock(fs);
+                    File.WriteAllText(file, JsonConvert.SerializeObject(stockSupport));
+                    stockManagement.DisplayStocks(fs);
+                    break;
+                case 2:
+                    stockManagement.DeleteInventory(fs);
+                    File.WriteAllText(file, JsonConvert.SerializeObject(stockSupport));
+                    stockManagement.DisplayStocks(fs);
+                    break;
+                case 3:
+                    stockManagement.DisplayStocks(fs);
+                    break;
+            }
+        }
+    }
+}
---
+++ StockAccountManagement/StockSupport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockAccountManagement
+{
+    class StockSupport
+    {
+        public List<Stocks> stocksList { get; set; }
+
+        public class Stocks
+        {
+            public string StockName { get; set; }
+            public int shares { get; set; }
+            public int Price { get; set; }
+        }
+    }
+}
